4aa17e53cab98b73f3271c293ed0a518c005ac7f
Alexandr Kovalchuk <skateray17@gmail.com>
2017-11-05 19:06:43 +0300

    a little bit visualisation

--- YouTubeTask/js/MainScript.js
+++ YouTubeTask/js/MainScript.js
@@ -2,6 +2,9 @@ const body = document.getElementById('body');
 const searchBarDiv = document.createElement('div');
 let videoName;
 let result;
+let videosDiv = document.createElement('div');
+
+videosDiv.className = 'videosDiv';
 
 searchBarDiv.innerHTML = `
 <input type="text" class="searchBox" placeholder = "Your request...">
@@ -13,6 +16,7 @@ searchBarDiv.className = 'searchBar';
 
 searchBarDiv.addEventListener('keypress', checkButton);
 body.appendChild(searchBarDiv);
+body.appendChild(videosDiv);
 
 function init() {
     gapi.client.setApiKey("AIzaSyBEx_BVKa13PtyxxAUC5jj2PCny_B-f5X4");
@@ -30,6 +34,7 @@ function checkButton(e){
 function searchButtonPressed(){
     videoName = document.getElementsByClassName("searchBox")[0].value;
     result = undefined;
+    videosDiv.innerHTML = '';
     sendRequest();
 }
 
@@ -42,8 +47,6 @@ function sendRequest(){
         pageToken: result ? result.nextPageToken : undefined,
         order: "viewCount",
     });
-
-   //console.log(request);
    
     request.execute(function(response) {
         result = response.result;
@@ -54,13 +57,42 @@ function sendRequest(){
         let request_stat = gapi.client.youtube.videos.list({ 
             id: tmp, 
             part: 'statistics', 
-        });  
+        });
+          
         request_stat.execute((responce_stat)=>{ 
           console.log(responce_stat.result);  
+          let statistics = responce_stat.result;
+          insertVideosFromResponse(statistics);          
         });
         console.log(result);
     });
 
 }
 
-let button = document.getElementsByClassName('searchButton')[0];
+function createVideo(index, stat){
+    let clip = document.createElement('div');
+    let video = result.items[index];
+    clip.className = 'clip';
+    clip.innerHTML = `
+    <img class = "clipPicture" src = ${video.snippet.thumbnails.medium.url}>
+    <a class = "clipRef" href = ${"https://www.youtube.com/embed/" + video.id.videoId}>
+        ${video.snippet.title}
+    </a>
+    <p>${video.snippet.description}</p>
+    <a class = "channelName" href = ${"https://www.youtube.com/channel/" + video.snippet.channelId}>
+        ${video.snippet.channelTitle}
+    </a>
+    <p>Publised at ${video.snippet.publishedAt}</p>
+    <p>View count: ${stat.items[index].statistics.viewCount}</p>
+    `;
+    return clip;
+    videosDiv.appendChild(clip);
+
+}
+
+function insertVideosFromResponse(statistics){
+    for(let i = 0; i < result.items.length; i++){
+        videosDiv.appendChild(createVideo(i, statistics));        
+    }
+}
+
